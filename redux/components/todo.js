35133456190193a7d816b44964d4a7d543ce4f62
hbbaly <1422699902@qq.com>
2018-12-03 22:02:12 +0800

    ADD: components of todo and todo list

---
+++ redux/components/todo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+class Todo extends React.Component{
+    constructor(){
+        super()
+    }
+    render(){
+        const props = this.props
+        return(
+            <li onClick = {props.handleClick} style={{textDecoration:props.completed?'line-through':'none'}}>
+                {props.text}
+            </li>
+        )
+    }
+}
+Todo.PropTypes = {
+    handleClick:PropTypes.func.isRequired,
+    completed:PropTypes.bool.isRequired,
+    text:PropTypes.string.isRequired
+}
+export default Todo
---
+++ redux/components/todoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Todo from './todo'
+class TodoList extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            
+        )
+    }
+}
