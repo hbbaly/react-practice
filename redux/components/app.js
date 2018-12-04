27768426301ff3addd605a2dc166573469162e15
hbbaly <1422699902@qq.com>
2018-12-04 23:07:27 +0800

    ADD：redux做简单的加减

--- redux/components/app.js
+++ redux/components/app.js
@@ -1,18 +1,43 @@
 import React from 'react'
-import FilterLink from './filterLink'
-import Footer from './footer'
-import AddTodo from './addTodo'
-import VisibilityTodoList from './visibilityTodoList'
+// import FilterLink from './filterLink'
+// import Footer from './footer'
+// import AddTodo from './addTodo'
+// import VisibilityTodoList from './visibilityTodoList'
+import {createStore} from 'redux'
+import reducer from '../store/reducers/index'
+const store = createStore(reducer)
 class App extends React.Component{
     constructor(props){
         super(props)
     }
+    componentDidMount(){
+        store.dispatch({type:'ADD'})
+        console.log(store.getState(),2)
+        store.dispatch({type:'ADD'})
+        console.log(store.getState(),3)
+        store.dispatch({type:'DESC'})
+        console.log(store.getState(),4)
+    }
+    handleAdd(){
+        store.dispatch({type:'ADD'})
+        console.log(store.getState(),'add')
+    }
+    handleDesc(){
+        store.dispatch({type:'DESC'})
+        console.log(store.getState(),'desc')
+    }
     render(){
         return(
             <div>
-                <AddTodo />
+                <button onClick={()=>this.handleAdd()}>
+                    加
+                </button>
+                <button onClick={()=>this.handleDesc()}>
+                    减
+                </button>
+                {/* <AddTodo />
                 <VisibilityTodoList />
-                <Footer />
+                <Footer /> */}
             </div>
         )
     }
--- redux/view/index.js
+++ redux/view/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import { createStore} from 'redux'
-import TodoApp from '../store/reducers'
-import App from './components/app'
-let store = createStore(TodoApp)
+// import {Provider} from 'react-redux'
+// import { createStore} from 'redux'
+// import TodoApp from '../store/reducers'
+import App from '../components/app'
+// let store = createStore(TodoApp)
+
+// ReactDOM.render(
+//   <Provider store = {store}>
+//     <App></App>
+//   </Provider>,
+//   document.getElementById("app")
+// );
 
 ReactDOM.render(
-  <Provider store = {store}>
-    <App></App>
-  </Provider>,
+  <App></App>,
   document.getElementById("app")
 );
