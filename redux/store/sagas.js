8a7fba798dfce70e7d9b15e6abb2f0cceb66a73a
hbbaly <1422699902@qq.com>
2018-12-08 13:35:21 +0800

    ADD：请求容错，

--- redux/store/sagas.js
+++ redux/store/sagas.js
@@ -3,10 +3,13 @@ import axios from 'axios'
 import creators from './creators'
 import actionTypes from './actionTypes'
 function* fetchList (){
-  const res = yield axios.get('https://www.vue-js.com/api/v1/topics')
-  const action = creators.getRequestData(res.data.data)
-  console.log(action)
-  yield put(action)
+  try{
+    const res = yield axios.get('https://www.vue-js.com/api/v1/topics')
+    const action = creators.getRequestData(res.data.data)
+    yield put(action)
+  }catch(e){
+    console.log('请求失败')
+  }
 }
 function* mySaga() {
   yield takeEvery(actionTypes.GETVUELIST, fetchList);
