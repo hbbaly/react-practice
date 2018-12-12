b0ee77e28e9271e714730345aea598a74081d638
hbbaly <1422699902@qq.com>
2018-12-12 13:39:02 +0800

    ADD：immutable避免对reducer数据直接修改

--- demo/jianshu/src/common/header/index.js
+++ demo/jianshu/src/common/header/index.js
@@ -39,7 +39,7 @@ const Header = (props) => {
   )
 }
 const mapStateToProps = (state) => ({
-  focused:state.header.focused
+  focused:state.header.get('focused')
 })
 const mapDispatchToProps = (dispatch) => ({
   handleFocus(){
--- demo/jianshu/src/common/header/store/reducers.js
+++ demo/jianshu/src/common/header/store/reducers.js
@@ -1,16 +1,19 @@
 import actionTypes from './actionTypies'
-
-const defaultState = {
+import {fromJS} from 'immutable'
+const defaultState = fromJS({
   focused:false
-}
+})
+console.log(defaultState)
 const headerReducer = (state=defaultState,action)=>{
-  const newState = JSON.parse(JSON.stringify(state))
+  // const newState = JSON.parse(JSON.stringify(state))
   if(action.type===actionTypes.Focused){
-    newState.focused = true
+    return state.set('focused',true)
+    // newState.focused = true
   }
   if(action.type===actionTypes.Blur){
-    newState.focused = false
+    return state.set('focused',false)
+    // newState.focused = false
   }
-  return newState
+  return state
 }
 export default headerReducer
