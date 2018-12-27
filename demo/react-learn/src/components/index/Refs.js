e0325ae25c99b5b3a1a966cb64dbdc747114f44d
hbbaly <1422699902@qq.com>
2018-12-27 13:54:08 +0800

    ADD: virtual dom 定义，渲染原理

--- demo/react-learn/src/components/index/Refs.js
+++ demo/react-learn/src/components/index/Refs.js
@@ -22,3 +22,7 @@ export default class Refs extends React.Component{
     )
   }
 }
+
+// 当 ref 属性被用于一个普通的 HTML 元素时，React.createRef() 将接收底层 DOM 元素作为它的 current 属性以创建 ref 。
+// 当 ref 属性被用于一个自定义类组件时，ref 对象将接收该组件已挂载的实例作为它的 current 。
+// 你不能在函数式组件上使用 ref 属性，因为它们没有实例。
--- demo/react-learn/src/views/index.js
+++ demo/react-learn/src/views/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
 import Typechecking from '../components/index/Typechecking'
 import Refs from '../components/index/Refs'
+import VirtualDom from '../components/index/VirtualDom'
 export default class Index extends React.Component{
   constructor(){
     super()
+    this.changeName = this.changeName.bind(this)
     this.state = {
       name:'hbbaly',
       age:20
     }
   }
+  changeName(){
+    this.setState({
+      name:'ly'
+    })
+  }
   render(){
     const  { name, age } = this.state
     return(
       <div className="page-index">
+      <button onClick = {this.changeName}>change name</button>
         <Typechecking name={name} age = {age}>
           <p>my name , my age </p>
         </Typechecking>
         <Refs />
+        <VirtualDom  name={name} />
       </div>
     )
   }
