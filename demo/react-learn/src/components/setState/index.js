2f0139f16ee76a3b7a96e722fa6534e338ad24b0
hbbaly <1422699902@qq.com>
2018-12-28 11:33:27 +0800

    ADD：setState讲解

--- demo/react-learn/src/components/setState/index.js
+++ demo/react-learn/src/components/setState/index.js
@@ -1,11 +1,40 @@
 import React from 'react'
 export default class SetStateView extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      choose:'',
+      arr:['first','second','third']
+    }
+  }
+  changeItem (val) {
+    // this.setState({
+    //   choose:val
+    // })
+    // console.log(this.state.choose)
+    // setState是异步函数，你在使用setState去改变某个值，他不会立刻改变，而是进入队列，某个时间点合并，一起刷新。，所以在使用setState改变某个值，立马去调用
+    // 有可能值还没有变化。
+
+    // 如何确定值发生了变化。setState有第二个参数，callback
+    // this.setState({
+    //   choose:val
+    // }, () => {
+    // console.log(this.state.choose,'choose is changed')
+    // })
+    setTimeout(() => {
+      this.setState({
+        choose:val
+      })
+      console.log(this.state.choose)
+    });
+
+  }
   render(){
     return(
       <div>
-        <p>first</p>
-        <p>second</p>
-        <p>third</p>
+        {this.state.arr.map(item => (
+          <p className='list' onClick={this.changeItem.bind(this,item)} key={item}>{item}</p>
+        ))}
       </div>
     )
   }
