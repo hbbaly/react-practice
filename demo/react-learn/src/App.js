70ad4bad3a156def4a96f4737736d9e4cf4f7246
hbbaly <1422699902@qq.com>
2018-12-28 12:00:17 +0800

    ADD: react 的 lifeCycle

--- demo/react-learn/src/App.js
+++ demo/react-learn/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Index from './views/index'
 import SetState from './views/setState'
+import LifeCycle from './views/lifeCycle'
 import './App.css'
 class App extends Component {
   jumpPath (path) {
@@ -13,8 +14,10 @@ class App extends Component {
         <div className="App">
             <Link className="list" to="/index">Index</Link>
             <Link className="list" to="/setstate">SetState</Link>
+            <Link className="list" to="/lifecycle">LifeCycle</Link>
             <Route path="/index" exact component={Index}></Route>
             <Route path="/setstate" component={SetState}></Route>
+            <Route path="/lifecycle" component={LifeCycle}></Route>
         </div>
       </Router>
     );
---
+++ demo/react-learn/src/views/lifeCycle.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import LifeCycleView from '../components/lifeCycle/index'
+export default class LifeCycle extends React.Component{
+  constructor () {
+    super ()
+    this.state = {
+      name:'hbb'
+    }
+  }
+  render(){
+    return(
+      <div className="page-lifecycle">
+        <LifeCycleView name= {this.state.name} />
+      </div>
+    )
+  }
+}
